95fd9de4ed70a69e9ddd7d84c4390db4fe010f61
pbokolo <dev.pbokolo@gmail.com>
2023-06-08 08:39:34 +0200

    Resetting the primary and on primary colors

--- src/ui/App.js
+++ src/ui/App.js
@@ -2,13 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import { Auth, Lost, Profile } from "./pages";
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route exact path="/" element={<Profile />} />
-        <Route exact path="/auth" element={<Auth />} />
-        <Route exact path="*" element={<Lost />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Profile />} />
+      <Route exact path="/auth" element={<Auth />} />
+      <Route exact path="*" element={<Lost />} />
+    </Routes>
   );
 }
 
