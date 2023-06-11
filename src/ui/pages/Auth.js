a84d2c9a1a9c93eabe92de1d6af29d80db312718
pbokolo <dev.pbokolo@gmail.com>
2023-06-11 09:20:20 +0200

    Final style touch

--- src/ui/pages/Auth.js
+++ src/ui/pages/Auth.js
@@ -1,10 +1,17 @@
 import React from "react";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+
 import SignInForm from "../components/SignInForm";
 
 export default function Auth() {
   return (
     <div className="page page__auth">
-      <h1>Sign in</h1>
+      <h1>
+        {<LockOutlinedIcon className="icon icon--big" fontSize="big" /> || (
+          <LockOpenOutlinedIcon className="icon icon--big" fontSize="big" />
+        )}
+      </h1>
       <SignInForm />
     </div>
   );
