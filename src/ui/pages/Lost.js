4337018312758ae3fc2d1734be57891111b346e9
pbokolo <dev.pbokolo@gmail.com>
2023-06-12 10:04:26 +0200

    Styling the lost page

--- src/ui/pages/Lost.js
+++ src/ui/pages/Lost.js
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 
 export default function Lost() {
   return (
-    <div className="page page__error">
+    <div className="page page__lost">
       <p>Ooops!</p>
-      <p>We can't found the ressource you're looking for </p>
-      <Link to="/">Go back home</Link>
+      <p>We can't find the ressource you're looking for </p>
+      <Link to="/" className="btn btn--primary btn--link">
+        Go back home
+      </Link>
     </div>
   );
 }
