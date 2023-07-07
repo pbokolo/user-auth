9d8f3f3a06af49ce52b5e631d5e62fca720d80d5
pbokolo <dev.pbokolo@gmail.com>
2023-07-07 12:58:11 +0200

    Final touch on the UI/UX

--- src/ui/components/AuthForm.js
+++ src/ui/components/AuthForm.js
@@ -4,8 +4,25 @@ import FormTextInput from "./FormTextInput";
 const initData = { email: "", password: "", secondPassword: "" };
 export default function AuthForm() {
   const [data, setData] = useState(initData);
+  const [authType, setAuthType] = useState("signin");
+
   const changeHandler = (e) => {
     setData({ ...data, [e.target.id]: e.target.value });
+  };
+
+  const authTypeChangeHandler = () => {
+    let newAuthType = "";
+    switch (authType) {
+      case "signin":
+        newAuthType = "signup";
+
+        break;
+      case "signup":
+        newAuthType = "signin";
+        break;
+    }
+
+    setAuthType(newAuthType);
   };
   return (
     <form className="auth">
@@ -23,6 +40,19 @@ export default function AuthForm() {
         value={data.password}
         changeHandler={changeHandler}
       />
+
+      {authType === "signup" ? (
+        <FormTextInput
+          id={"secondPassword"}
+          label="Retype password"
+          type="password"
+          value={data.secondPassword}
+          changeHandler={changeHandler}
+        />
+      ) : (
+        ""
+      )}
+
       <input
         type="submit"
         value={"Sign in"}
@@ -31,8 +61,12 @@ export default function AuthForm() {
 
       <div className="auth__signup-cta">
         <p className="text">
-          Don't have an account?{" "}
-          <span className="text--cta-signup">Sign up</span>
+          {authType === "signin"
+            ? "Don't have an account?"
+            : "Have an account?"}{" "}
+          <span className="text--cta-signup" onClick={authTypeChangeHandler}>
+            {authType === "signin" ? "Signup" : "Signin"}{" "}
+          </span>
         </p>
       </div>
     </form>
