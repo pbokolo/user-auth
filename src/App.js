b641e715d9434f3de29142613f06b50c5a01b823
pbokolo <dev.pbokolo@gmail.com>
2023-07-09 12:49:26 +0200

    Final touch on the style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import { Auth, Home, Lost } from "./ui/pages";
+import { Home, Lost } from "./ui/pages";
 function App() {
   return (
     <div className="app">
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/auth" element={<Auth />} />
+
         <Route exact path="*" element={<Lost />} />
       </Routes>
     </div>
--- src/ui/pages/index.js
+++ src/ui/pages/index.js
@@ -1,5 +1,4 @@
-import Auth from "./Auth";
 import Home from "./Home";
 import Lost from "./Lost";
 
-export { Auth, Home, Lost };
+export { Home, Lost };
