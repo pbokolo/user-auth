576e08db673f85f5765bb3a537a3dd16eb40c96f
pbokolo <dev.pbokolo@gmail.com>
2023-07-09 12:16:33 +0200

    Polishing the Auth UX

--- src/ui/components/AuthDialog.js
+++ src/ui/components/AuthDialog.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import AuthForm from "./AuthForm";
 import { authController } from "../../controller/auth";
+import CloseIcon from "@mui/icons-material/Close";
 
-export default function AuthDialog({ show }) {
+export default function AuthDialog({ show, closeHandler }) {
   const { TYPE_SIGN_IN } = authController;
   const [authType, setAuthType] = useState(TYPE_SIGN_IN);
   const title = authType === TYPE_SIGN_IN ? "Sign in" : "Create new account";
@@ -11,9 +12,12 @@ export default function AuthDialog({ show }) {
     authController.handleAuthTypeChange(authType, setAuthType, errorsSetter);
   };
   return (
-    <div className="dialog__overlay">
+    <div id="overlay" className="dialog__overlay" onClick={closeHandler}>
       <dialog open={show} className="dialog__dialog">
         <h1 className="heading heading--2">{title}</h1>
+        <div className="dialog__btn-close btn btn--close" id="close">
+          <CloseIcon fontSize="large" id="close__icon" />
+        </div>
         <div className="content">
           <AuthForm type={authType} authTypeChange={authTypeChangeHandler} />
         </div>
--- src/ui/pages/Home.js
+++ src/ui/pages/Home.js
@@ -6,9 +6,37 @@ export default function Home() {
   const [user, setUser] = useState(null);
   const [showAuthDialog, setAuthDialog] = useState(!user);
 
+  const handleCloseAuthDialog = (e) => {
+    if (
+      e.target.id !== "overlay" &&
+      e.target.id !== "close" &&
+      e.target.id !== "close__icon"
+    )
+      return;
+    setAuthDialog(false);
+  };
+
+  const handleOpenAuthDialog = () => {
+    setAuthDialog(true);
+  };
+
   return (
     <div className="page">
-      {user ? "Welcome home" : <AuthDialog show={showAuthDialog} />}
+      {user ? (
+        "Welcome home"
+      ) : (
+        <div className="btn btn--primary" onClick={handleOpenAuthDialog}>
+          sign in to continue
+        </div>
+      )}
+      {showAuthDialog ? (
+        <AuthDialog
+          show={showAuthDialog}
+          closeHandler={handleCloseAuthDialog}
+        />
+      ) : (
+        ""
+      )}
     </div>
   );
 }
