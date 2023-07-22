90f6c1f901a9d9f68fdb1c711b8b95cb78400ed8
pbokolo <dev.pbokolo@gmail.com>
2023-07-22 21:11:39 +0200

    Error helpers

---
+++ src/helpers/errors.js
@@ -0,0 +1,7 @@
+export const authErrors = {
+  "auth/user-not-found": "Cet email est incorrect.",
+  "auth/wrong-password": "Mot de passe érroné.",
+  "auth/email-already-in-use":
+    "Cet email est déjà utilisé. Veuillez choisir un autre.",
+  "no-equal-pass": "Les deux mots de passe sont différents.",
+};
