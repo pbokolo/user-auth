5c50322c7c7e182c1e2d2d9e09e76d7513fbfb2e
pbokolo <dev.pbokolo@gmail.com>
2023-07-25 20:58:16 +0200

    Picture upload

--- src/ui/components/UserCard.js
+++ src/ui/components/UserCard.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import dog from "../../assets/dog.jpg";
 import { authController } from "../../controller/auth";
 
 export default function UserComponent({ user }) {
+  const [uploadedPicture, setUploadedPicture] = useState(null);
+  const handlePictureUpload = (e) => {
+    console.log(e.target.files[0]);
+    setUploadedPicture(URL.createObjectURL(e.target.files[0]));
+  };
   return (
     <div className="card card--user">
       <div className="card--user__photo">
-        <img src={dog} alt="user" className="card--user__photo-picture" />
+        <img
+          src={uploadedPicture || dog}
+          alt="user"
+          className="card--user__photo-picture"
+        />
+        <input
+          className="card--user__photo-btn"
+          type="file"
+          onChange={handlePictureUpload}
+        />
       </div>
       <div className="card--user__identity">
         <ul>
