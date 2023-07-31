9e2cdefdcd7c85da5d009fd194003ae537b281c6
pbokolo <dev.pbokolo@gmail.com>
2023-07-31 12:41:57 +0200

    Properties update

--- src/ui/components/UserCard.js
+++ src/ui/components/UserCard.js
@@ -2,12 +2,35 @@ import React, { useState } from "react";
 import PhotoOutlinedIcon from "@mui/icons-material/PhotoOutlined";
 import dog from "../../assets/dog.jpg";
 import { authController } from "../../controller/auth";
+import UpdateForm from "./UpdateForm";
 
 export default function UserComponent({ user }) {
   const [profilePictureUrl, setProfilePictureUrl] = useState(user.photoURL);
+  const [usrnameEdit, setUsernameEdit] = useState(false);
+  const [emailEdit, setEmailEdit] = useState(false);
+  const [phoneEdit, setPhoneEdit] = useState(false);
+
   const handlePictureUpload = (e) => {
     const file = e.target.files[0];
     authController.handleProfilePictureSubmit(file, setProfilePictureUrl);
+  };
+
+  const handleClickIdentity = (e) => {
+    switch (e.target.id) {
+      case "username":
+        setUsernameEdit(true);
+        break;
+      case "email":
+        setEmailEdit(true);
+        break;
+      case "phone":
+        setPhoneEdit(true);
+        break;
+    }
+  };
+
+  const updateSubmitHandler = (e) => {
+    e.preventDefault();
   };
 
   return (
@@ -38,13 +61,22 @@ export default function UserComponent({ user }) {
           />
         </div>
       </div>
-      <div className="card--user__identity">
-        <ul>
-          <li>{user.displayName || "To be defined"}</li>
-          <li>{user.email || "To be defined"}</li>
-          <li>{user.phoneNumber || "To be defined"}</li>
-          <li>Address</li>
-        </ul>
+      <div className="card--user__identity" onClick={handleClickIdentity}>
+        {usrnameEdit ? (
+          <UpdateForm submitHandler={updateSubmitHandler} />
+        ) : (
+          <p id="username">{user.displayName || "To be defined"}</p>
+        )}
+        {emailEdit ? (
+          <UpdateForm submitHandler={updateSubmitHandler} />
+        ) : (
+          <p id="email">{user.email || "To be defined"}</p>
+        )}
+        {phoneEdit ? (
+          <UpdateForm submitHandler={updateSubmitHandler} />
+        ) : (
+          <p id="phone">{user.phoneNumber || "To be defined"}</p>
+        )}
       </div>
       <div className="card--user__actions">
         <button
